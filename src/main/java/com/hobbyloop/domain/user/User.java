c1f176223b65ca3591a1e49a8591f53b4a63e662
Limjongho <whdgh9873@icloud.com>
2023-03-17 18:53:41 +0900

    fix: 개행문자 수정

--- src/main/java/com/hobbyloop/domain/user/User.java
+++ src/main/java/com/hobbyloop/domain/user/User.java
@@ -1,1 +1,29 @@
-package com.hobbyloop.domain.user;import javax.persistence.*;import java.time.LocalDate;@Entitypublic class User {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long userId;    private String password;    @Enumerated(value = EnumType.STRING)    private AuthProvider authProvider;    private String socialEmail;    @Enumerated(value = EnumType.STRING)    private UserStatus userStatus;    private String nickname;    private String profileImgUrl;    private String introduction;    private int point;    private String account;    private LocalDate agreement;}
+package com.hobbyloop.domain.user;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private AuthProvider authProvider;
+    private String socialEmail;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserStatus userStatus;
+
+    private String nickname;
+    private String profileImgUrl;
+    private String introduction;
+    private int point;
+    private String account;
+    private LocalDate agreement;
+
+}

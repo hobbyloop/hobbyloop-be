fcbea0043124901b87cf01efc4e118c36630a814
jungminji <wjdalswl0215@gmail.com>
2023-03-17 15:39:12 +0900

    initial commit

---
+++ src/main/java/com/hobbyloop/HobbyloopApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobbyloop/HobbyloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

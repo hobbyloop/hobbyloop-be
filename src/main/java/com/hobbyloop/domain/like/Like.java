30bfcce323055d01567e30bf4a3bd5a46e03a58d
Limjongho <whdgh9873@icloud.com>
2023-03-23 16:49:11 +0900

    feat: Like(리뷰에 좋아요)  엔티티 생성

---
+++ src/main/java/com/hobbyloop/domain/like/Like.java
@@ -0,0 +1,23 @@
+package com.hobbyloop.domain.like;
+
+import com.hobbyloop.domain.review.Review;
+import com.hobbyloop.domain.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "reviewId")
+    private Review review;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+}

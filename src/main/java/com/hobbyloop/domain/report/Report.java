84039fcee0a0828e177a918b58593832861238d9
Limjongho <whdgh9873@icloud.com>
2023-04-07 09:32:38 +0900

    fix(Report): 신고 ID 값 전략 수정

--- src/main/java/com/hobbyloop/domain/report/Report.java
+++ src/main/java/com/hobbyloop/domain/report/Report.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Report extends BaseTime {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reportId;
 
     @ManyToOne

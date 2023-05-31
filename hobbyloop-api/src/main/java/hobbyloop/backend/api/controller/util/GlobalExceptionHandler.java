e0c15056b0979f31847943a9cb60fca5c116ba96
Limjongho <whdgh9873@icloud.com>
2023-05-31 13:10:05 +0900

    fix : 로깅 메시지 출력 안되는 부분 수정 및 에러 타입 상수화

--- hobbyloop-api/src/main/java/hobbyloop/backend/api/controller/util/GlobalExceptionHandler.java
+++ hobbyloop-api/src/main/java/hobbyloop/backend/api/controller/util/GlobalExceptionHandler.java
@@ -11,13 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final ExceptionResponse ENUM_BINDING_EXCEPTION_RESPONSE =
+            ExceptionResponse.from(ExceptionStatus.ENUM_TYPE_NOT_FOUND);
+
     /**
      * enum Type 불일치 Exception
      */
     @ExceptionHandler(EnumTypeBindingException.class)
     protected ResponseEntity<ExceptionResponse> handle(EnumTypeBindingException error) {
-        log.error("handle error", error);
-        final ExceptionResponse response = ExceptionResponse.from(ExceptionStatus.ENUM_TYPE_NOT_FOUND);
-        return new ResponseEntity<>(response, ExceptionStatus.ENUM_TYPE_NOT_FOUND.getHttpStatus());
+        log.error("handle error : {}", error.getMessage());
+        return new ResponseEntity<>(ENUM_BINDING_EXCEPTION_RESPONSE, ExceptionStatus.ENUM_TYPE_NOT_FOUND.getHttpStatus());
     }
 }

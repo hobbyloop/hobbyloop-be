b40c495e8827b870ff247bd869fd66c5098de0ac
Limjongho <whdgh9873@icloud.com>
2023-06-03 13:23:33 +0900

    refactor: 다수 파라미터를 DTO 로 변경

--- hobbyloop-api/src/main/java/hobbyloop/backend/api/controller/center/CenterController.java
+++ hobbyloop-api/src/main/java/hobbyloop/backend/api/controller/center/CenterController.java
@@ -1,7 +1,9 @@
 package hobbyloop.backend.api.controller.center;
 
 import hobbyloop.backend.api.applicationservice.center.CenterApplicationService;
+import hobbyloop.backend.api.controller.center.dto.CenterDistanceListRequestDTO;
 import hobbyloop.backend.api.controller.center.dto.CenterListResponseDTO;
+import hobbyloop.backend.api.controller.center.dto.CenterRankingListRequestDTO;
 import hobbyloop.backend.api.infra.util.ApiResponse;
 import hobbyloop.backend.domain.center.CenterSortType;
 import hobbyloop.backend.domain.ticket.TicketType;
@@ -12,10 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,43 +28,38 @@ public class CenterController {
     @ApiOperation(value = "업체 리스트 랭킹순으로 조회",
             notes = "ticketType : Figma 내의 이용권 카테고리 그대로 설정 && sortType(정렬기준) : recently(=최신순), amount(=판매량순), score(=평점순)")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "유저 식별자", dataType = "Long", dataTypeClass = Long.class),
-            @ApiImplicitParam(name = "ticketType", value = "이용권의 카테고리", dataType = "String", dataTypeClass = TicketType.class),
-            @ApiImplicitParam(name = "sortType", value = "정렬기준", dataType = "String", dataTypeClass = CenterSortType.class),
             @ApiImplicitParam(name = "page", value = "페이지 번호", dataTypeClass = Integer.class),
             @ApiImplicitParam(name = "size", value = "페이지당 데이터 개수", dataTypeClass = Integer.class)
     })
     @GetMapping("/list/ranking")
     public ApiResponse<List<CenterListResponseDTO>> getCentersWithRanking(
-            @RequestParam Long userId,
-            @RequestParam String ticketType,
-            @RequestParam String sortType,
+            @RequestBody CenterRankingListRequestDTO centerRankingListRequestDTO,
             Pageable pageable
     ) {
         return ApiResponse.success(HttpStatus.OK,
-                centerApplicationService.getCentersWithRanking(userId, ticketType, sortType, pageable));
+                centerApplicationService.getCentersWithRanking(
+                        centerRankingListRequestDTO.getUserId(),
+                        centerRankingListRequestDTO.getTicketType(),
+                        centerRankingListRequestDTO.getSortType(), pageable));
     }
 
     @ApiOperation(value = "이용권 리스트 거리순으로 조회",
             notes = "ticketType : Figma 내의 이용권 카테고리 그대로 설정")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "유저 식별자", dataType = "Long", dataTypeClass = Long.class),
-            @ApiImplicitParam(name = "ticketType", value = "이용권의 카테고리", dataType = "String", dataTypeClass = TicketType.class),
-            @ApiImplicitParam(name = "mapx", value = "경도", dataTypeClass = Integer.class),
-            @ApiImplicitParam(name = "mapy", value = "위도", dataTypeClass = Integer.class),
             @ApiImplicitParam(name = "page", value = "페이지 번호", dataTypeClass = Integer.class),
             @ApiImplicitParam(name = "size", value = "페이지당 데이터 개수", dataTypeClass = Integer.class)
     })
     @GetMapping("/list/distance")
     public ApiResponse<List<CenterListResponseDTO>> getTicketsWithDistance(
-            @RequestParam Long userId,
-            @RequestParam String ticketType,
-            @RequestParam double mapx,
-            @RequestParam double mapy,
+            @RequestBody CenterDistanceListRequestDTO centerDistanceListRequestDTO,
             Pageable pageable
     ) {
         return ApiResponse.success(HttpStatus.OK,
-                centerApplicationService.getCentersWithDistance(userId, ticketType, mapx, mapy, pageable));
+                centerApplicationService.getCentersWithDistance(
+                        centerDistanceListRequestDTO.getUserId(),
+                        centerDistanceListRequestDTO.getTicketType(),
+                        centerDistanceListRequestDTO.getMapx(),
+                        centerDistanceListRequestDTO.getMapy(), pageable));
     }
 
 }

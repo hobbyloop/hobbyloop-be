acd7e2c15164415a78e89633a504e6fc5a669311
road3144 <fnfnfn258@gmail.com>
2023-06-09 17:56:23 +0900

    feat: add login Exception Handler, comment Exception Handler

--- hobbyloop-domain/src/main/java/hobbyloop/backend/domain/exception/unexpectedURL/UnExpectedURLException.java
+++ hobbyloop-domain/src/main/java/hobbyloop/backend/domain/exception/unexpectedURL/UnExpectedURLException.java
@@ -3,8 +3,8 @@ package hobbyloop.backend.domain.exception.unexpectedURL;
 import hobbyloop.backend.domain.exception.BusinessException;
 import hobbyloop.backend.domain.exception.ExceptionStatus;
 
-public class UnExpectedLoginURL extends UnExpectedURLException {
-	public UnExpectedLoginURL() {
-		super(ExceptionStatus.CANNOT_EXTRACT_SOCIAL_NAME_IN_URL);
+public class UnExpectedURLException extends BusinessException {
+	public UnExpectedURLException(ExceptionStatus exceptionStatus) {
+		super(exceptionStatus);
 	}
 }

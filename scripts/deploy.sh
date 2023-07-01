ad8cab31f34e0bcb1eb88f07e341579ec1c17329
Limjongho <whdgh9873@icloud.com>
2023-07-01 19:22:21 +0900

    fix: deploy script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,7 +38,6 @@ then
   echo "> 현재 동작중인 어플리케이션 존재 X" >> $API_DEPLOY_LOG_PATH
 else
   echo "> 현재 동작중인 어플리케이션 존재 O" >> $API_DEPLOY_LOG_PATH
-  echo "> 현재 동작중인 어플리케이션 kill -15 옵션으로 종료 실패 -> kill -9 옵션 실행" >> $API_DEPLOY_LOG_PATH
   echo "> kill -9 $CURRENT_PID" >> $API_DEPLOY_LOG_PATH
   kill -9 $CURRENT_PID
 fi
@@ -48,6 +47,7 @@ while [ -n "$CURRENT_PID" ]; do
     sleep 1
 done
 
+echo "> 현재 동작중인 어플리케이션 존재 X -> 배포 시작"
 # 배포
 DEPLOY_API_JAR=$API_DEPLOY_PATH$API_JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $API_DEPLOY_LOG_PATH
@@ -71,7 +71,6 @@ then
   echo "> 현재 동작중인 어플리케이션 존재 X" >> $ADMIN_DEPLOY_LOG_PATH
 else
   echo "> 현재 동작중인 어플리케이션 존재 O" >> $ADMIN_DEPLOY_LOG_PATH
-  echo "> 현재 동작중인 어플리케이션 kill -15 옵션으로 종료 실패 -> kill -9 옵션 실행" >> $ADMIN_DEPLOY_LOG_PATH
   echo "> kill -9 $CURRENT_PID" >> $ADMIN_DEPLOY_LOG_PATH
   kill -9 $CURRENT_PID
 fi
@@ -80,6 +79,8 @@ fi
 while [ -n "$CURRENT_PID" ]; do
     sleep 1
 done
+
+echo "> 현재 동작중인 어플리케이션 존재 X -> 배포 시작"
 
 DEPLOY_ADMIN_JAR=$ADMIN_DEPLOY_PATH$ADMIN_JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $ADMIN_DEPLOY_LOG_PATH
